7809d4fb69edd8a9ded86afa1b72c86d9a071c34
Math Costa <xcudder@hotmail.com>
2023-06-11 17:48:13 -0300

    Fixes ox inventory integration

--- client/main.lua
+++ client/main.lua
@@ -37,17 +37,17 @@ function check_for_store_props()
 	local shop_type = ''
 	local shop_prop, close_by_shop = false, false
 
-	for is = 1, #Config.shop_types do
-		shop_type = Config.shop_types[is]
+	for type_index = 1, #Config.shop_types do
+		shop_type = Config.shop_types[type_index]
 
-		for i = 1, #Config[shop_type] do
-			shop_prop = GetClosestObjectOfType(x, y, z, 0.85, Config[shop_type][i])
+		for prop_index = 1, #Config[shop_type] do
+			shop_prop = GetClosestObjectOfType(x, y, z, 0.85, Config[shop_type][prop_index])
 
 			if (shop_prop ~= 0) then
-				if ( not current_shop_id or current_shop_id ~= p_to_id(shop_prop)) then --no shop or new shop
+				if not current_shop_id or (current_shop_id ~= p_to_id(shop_prop)) then --no shop or new shop
 					close_by_shop = get_or_create_shop(shop_prop, shop_type)
 					current_shop_id = close_by_shop
-				elseif (current shop and current_shop_id == p_to_id(shop_prop)) then -- same shop
+				elseif current_shop and (current_shop_id == p_to_id(shop_prop)) then -- same shop
 					close_by_shop = current_shop_id
 				end
 			end
@@ -59,12 +59,12 @@ end
 
 function get_or_create_shop(shop_prop, shop_type)
 	local id = p_to_id(shop_prop)
-	debug_message("get or create shop (" .. id .. ")", 500)
+	debug_message("get or create shop (" .. id .. ")")
 	if created_shops[id] then return id end
 
 	ESX.TriggerServerCallback('esx_model_shops:register_shop', function(success)
 		created_shops[id] = true
-	end, id, Config[shop_type .. "_items"], GetEntityCoords(shop_prop)) --id, inventory and coords
+	end, id, Config[shop_type .. "_items"]) --id, inventory and coords
 
 	if not created_shops[id] then Wait(100) end -- wait on callback
 
@@ -89,5 +89,5 @@ function DisplayHelpText(str)
 end
 
 function p_to_id(shop_prop)
-	"id_" .. tostring(shop_prop)
+	return "id_" .. tostring(shop_prop)
 end
--- server/main.lua
+++ server/main.lua
@@ -1,10 +1,9 @@
-ESX.RegisterServerCallback("esx_model_shops:register_shop", function(source, cb, id, inventory, v3)
+ESX.RegisterServerCallback("esx_model_shops:register_shop", function(source, cb, id, inventory)
 	if GetResourceState('ox_inventory') == 'started' then
 		exports.ox_inventory:RegisterShop(id, {
 		    id = id,
 		    name = 'Stand / Vending Machine',
 		    inventory = inventory,
-		    locations = { v3 },
 		})
 	end
 	cb(true)
